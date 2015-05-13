8f153c9b0f0f34670453c0d4b1de97f7bbcc69d5
Rafael Navarro <rafa.n.ocon@gmail.com>
2015-05-13 10:52:18 +0200

    Initial Commit

---
+++ PracticaSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticaSwift
+//
+//  Created by Rafael Navarro on 13/5/15.
+//  Copyright (c) 2015 Rafa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
